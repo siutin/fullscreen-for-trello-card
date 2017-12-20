93dd8eac7b204d6d6bbb3463f70d4544143b9192
martin <osiutino@gmail.com>
2017-12-20 23:37:43 +0800

    make card full screen

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 console.log("background.js")
 
 chrome.browserAction.onClicked.addListener(function(tab) {
- chrome.tabs.executeScript(null,{file:'main.js',allFrames:false});
+  chrome.tabs.executeScript(null,{file:'jquery.js',allFrames:false});
+  chrome.tabs.executeScript(null,{file:'main.js',allFrames:false});
 })
--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("main.js")
+jQuery(function () {
+  jQuery(".window").css({width: "100%", "margin": "0"})
+  jQuery(".window-main-col").css({"width": "100%", "padding": "0", "margin": "0"})
+  jQuery(".card-detail-data.u-gutter").css({"margin-left": "0"})
+  jQuery(".card-detail-item.card-detail-item-block.u-clearfix.editable").css({"margin": "0"})
+})
